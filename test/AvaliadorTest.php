86c1db96f0485ecb64fb1d64ab189174b4387a26
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-06-20 01:06:46 -0400

    Aula 2 - Atividade 1 - Inserindo o teste de lances em ordens Aleatórias

--- test/AvaliadorTest.php
+++ test/AvaliadorTest.php
@@ -54,4 +54,30 @@
 			$this->assertEquals($menorEsperado, $leiloeiro->getMenorLance(), 0.00001);
 			$this->assertEquals($maiorEsperado, $leiloeiro->getMaiorLance(), 0.00001);	
 		}
+
+		public function testDeveAceitarLancesEmOrdemAleatoria() {
+			$u1 = new Usuario("U1");
+			$u2 = new Usuario("U2");
+			$u3 = new Usuario("U3");
+			$u4 = new Usuario("U4");
+			$u5 = new Usuario("U5");
+			$u6 = new Usuario("U6");
+
+			$leilao = new Leilao("Uma bola de Tênis");
+			$leilao->propoe(new Lance($u1, 300));
+			$leilao->propoe(new Lance($u2, 200));
+			$leilao->propoe(new Lance($u3, 454));
+			$leilao->propoe(new Lance($u4, 190));
+			$leilao->propoe(new Lance($u5, 147));
+			$leilao->propoe(new Lance($u6, 302));
+
+			$leiloeiro = new Avaliador();
+			$leiloeiro->avalia($leilao);
+
+
+			$menorEsperado = 147;
+			$maiorEsperado = 454;
+			$this->assertEquals($menorEsperado, $leiloeiro->getMenorLance(), 0.00001);
+			$this->assertEquals($maiorEsperado, $leiloeiro->getMaiorLance(), 0.00001);
+		}
 	}

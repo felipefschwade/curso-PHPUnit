1fde2c76edd12cda9d16624b36b14a1b99e95504
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-06-20 22:25:28 -0400

    Aula 2 - Atividade 10 - Implementando a classe FiltraLances e testando todos os seus cenários

---
+++ FiltroDeLances.php
@@ -0,0 +1,21 @@
+<?php
+    require_once "Leilao.php";
+    require_once "Usuario.php";
+    require_once "Lance.php";
+    class FiltroDeLances {
+
+        public function filtra(Array $lances) {
+            $resultado = [];
+
+            foreach($lances as $lance) {
+                if($lance->getValor() > 1000 && $lance->getValor() < 3000) 
+                        $resultado[] = $lance;
+                else if($lance->getValor() > 500 && $lance->getValor() < 700) 
+                        $resultado[] = $lance;
+                else if($lance->getValor() > 5000) 
+                        $resultado[] = $lance;
+            }
+
+            return $resultado;
+        }
+    }
---
+++ test/FiltroDeLancesTest.php
@@ -0,0 +1,62 @@
+<?php
+    require_once "../FiltroDeLances.php";
+    class FiltroDeLancesTest extends PHPUnit_Framework_TestCase {
+
+        public function testDeveSelecionarLancesEntre1000E3000() {
+            $joao = new Usuario("Joao");
+
+            $filtro = new FiltroDeLances();
+            $lances = [];
+            $lances[] = new Lance($joao,2000);
+            $lances[] = new Lance($joao,1000);
+            $lances[] = new Lance($joao,3000);
+            $lances[] = new Lance($joao,800);
+
+            $resultado = $filtro->filtra($lances);
+
+            $this->assertEquals(1, count($resultado));
+            $this->assertEquals(2000, $resultado[0]->getValor(), 0.00001);
+        }
+
+        public function testDeveSelecionarLancesEntre500E700() {
+            $joao = new Usuario("Joao");
+
+            $filtro = new FiltroDeLances();
+            $lances = [];
+            $lances[] = new Lance($joao,600);
+            $lances[] = new Lance($joao,500);
+            $lances[] = new Lance($joao,700);
+            $lances[] = new Lance($joao,800);
+
+            $resultado = $filtro->filtra($lances);
+            $this->assertEquals(1, count($resultado));
+            $this->assertEquals(600, $resultado[0]->getValor(), 0.00001);
+        }
+
+         public function testDeveEliminarOsLancesMaioresQue3000EMenoresQue5000ETambemOsMenoresQue500() {
+            $joao = new Usuario("Joao");
+
+            $filtro = new FiltroDeLances();
+            $lances = [];
+            $lances[] = new Lance($joao,3000);
+            $lances[] = new Lance($joao,400);
+
+            $resultado = $filtro->filtra($lances);
+            $this->assertEquals(0, count($resultado));
+            
+        }
+
+        public function testDeveSelecionarLanceMaiorQue5000() {
+            $joao = new Usuario("Joao");
+
+            $filtro = new FiltroDeLances();
+            $lances = [];
+            $lances[] = new Lance($joao,3000);
+            $lances[] = new Lance($joao,400);
+            $lances[] = new Lance($joao, 5001);
+
+            $resultado = $filtro->filtra($lances);
+            $this->assertEquals(1, count($resultado));
+            $this->assertEquals(5001, $resultado[0]->getValor(), 0.00001);
+        }
+    }
